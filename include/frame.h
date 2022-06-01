42b361e090bf09791e20ad01bde7b5271888dbeb
Dong thanh Tran <dtran@f1r6s3.codam.nl>
2022-06-01 16:08:35 +0200

    frame.h

--- include/frame.h
+++ include/frame.h
@@ -1,8 +1,17 @@
 
 #ifndef FRAME_H
 # define FRAME_H
-struct frame {
-	
 
-};
+typedef enum e_type {
+	NOTHING,
+	SPARE,
+	STRIKE
+} type;
+
+typedef struct s_frame {
+	int	throw_1;
+	int	throw_2;
+	int	throw_3;
+	type throw_type;
+}	t_frame;
 #endif
